d539797a712fc1213f6cb7c8823312a8ad330d65
Rafael Guimarães <75204221+RafaGui@users.noreply.github.com>
2024-02-08 17:12:44 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-const nome = document.getElementById('name');
-const email = document.getElementById('email');
 const btn = document.getElementById('send');
-
-btn.addEventListener('click', () => {
-    const texto = document.getElementById('texto').value;
-    const url = "https://api.whatsapp.com/send?phone=5521964124256&text=Olá, meu nome é " + nome.value + ".";
+btn.addEventListener('click', (event) => {
+    const nome = document.getElementById('name').value;
+	const email = document.getElementById('email').value;
+	const text = document.getElementById('text').value;
+	
+    const url = "https://api.whatsapp.com/send?phone=5521964124256&text=Olá, meu nome é " + nome + " e meu e-mail é o " + email + " o motivo do meu contato é: " + text;
     window.location.href = url;
+	event.preventDefault();
 });
