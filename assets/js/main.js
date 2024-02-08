17a90015415bc453bbc88fa4ada4b9ccf71bbf05
Rafael Guimarães <75204221+RafaGui@users.noreply.github.com>
2024-02-08 16:55:29 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,9 @@
-const myObserver = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if(entry.isIntersecting){
-			entry.target.classList.add('show')
-		}else{
-			entry.target.classList.remove('show')
-		}
-	});
+const nome = document.getElementById('name');
+const email = document.getElementById('email');
+const btn = document.getElementById('send');
+
+btn.addEventListener('click', () => {
+    const texto = document.getElementById('texto').value;
+    const url = "https://api.whatsapp.com/send?phone=5521964124256&text=Olá, meu nome é " + nome.value + ".";
+    window.location.href = url;
 });
-
-const elements = document.querySelectorAll('.hidden');
-
-// elements.forEach((element) => { myObserver.observe(element)});
-console.log(elements);
